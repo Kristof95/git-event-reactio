8675035e529d8d801818e9797be89f594c191f73
Kristóf Bodnár <Kristof.Bodnar@wup.hu>
2016-12-22 12:16:51 +0100

    modified .gitignore

--- logger.py
+++ logger.py
@@ -3,21 +3,17 @@ import csv
 
 
 class Logger:
-
+    
     log_file = open("logs/log.csv", "a")
     fieldnames = ('Date', 'Event type', 'Commit message')
     writer = csv.DictWriter(log_file, fieldnames)
     headers = dict((n, n) for n in fieldnames)
 
     @staticmethod
-    def setup_hearders():
+    def setup_headers():
         Logger.writer.writerow(Logger.headers)
 
     @staticmethod
     def logging(event_type, commit_message):
-        try:
-            Logger.writer.writerow({'Date': str(datetime.now()),
-                                'Event type': event_type,
-                                'Commit message': commit_message})
-        finally:
-            Logger.log_file.close()
+            Logger.writer.writerow({'Date': str(datetime.now()), 'Event type': event_type,
+                                    'Commit message': commit_message})
--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from logger import Logger
 
 if __name__ == '__main__':
     w = Watcher()
-    l = Logger()
-    l.setup_hearders()
+    Logger.setup_headers()
     w.run()
